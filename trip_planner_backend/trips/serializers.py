995159ede8e843d1fa3ec41ffd1d6276d7877476
LekakenyCollins <lekakenycollins8@gmail.com>
2025-03-26 14:29:41 +0300

    Add serializers for Trip, Stop, LogEntry, and Route models

---
+++ trip_planner_backend/trips/serializers.py
@@ -0,0 +1,50 @@
+from rest_frmaework import serializers
+from trips.models import Stop, Trip, LogEntry, Route
+
+class StopSerializer(serializers.ModelSerializer):
+    """Serializer for stop model"""
+    class Meta:
+        model = Stop
+        fields = ['id', 'trip', 'location', 'stop_type', 'order', 'arrival_time', 'departure_time', 'duration']
+        read_only_fields = ['id', 'duration']
+
+class LogEntrySerializer(serializers.ModelSerializer):
+    """Serializer for log entry model"""
+    class Meta:
+        model = LogEntry
+        fields = ['id', 'trip', 'date', 'status', 'start_time', 'end_time', 'duration', 'remarks']
+        read_only_fields = ['id', 'duration']
+
+    def validate(self, data):
+        """Ensures that the log entries for a day do not exceed 24 hours"""
+        trip = data.get('trip')
+        log_date = data.get('date')
+        exisiting_logs = LogEntry.objects.filter(trip=trip, date=log_date)
+
+        # Compute total time including new entry
+        total_duration = sum([log.duration for log in exisiting_logs], data.get('end_time') - data.get('start_time'))
+
+        if total_duration > timedelta(hours=24):
+            raise serializers.ValidationError("Total duration for the day cannot exceed 24 hours")
+
+        return data
+
+class TripSerializer(serializers.ModelSerializer):
+    """Serializer for trip model with nested stops and logs"""
+
+    stops = StopSerializer(many=True, read_only=True) # Nested stops
+    log_entries = LogEntrySerializer(many=True, read_only=True) # Nested log entries
+
+    class Meta:
+        model = Trip
+        fields = ['id', 'current_location', 'pickup_location', 'dropoff_location', 'current_cycle_hours', 'estimated_distance', 'estimated_duration', 'status', 'created_at', 'updated_at', 'stops', 'log_entries']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+class RouteSerializer(serializers.ModelSerializer):
+    """Serializer for route model"""
+
+    class Meta:
+        model = Route
+        fields = ['id', 'trip', 'route_data', 'created_at']
+        read_only_fields = ['id', 'created_at']
+        
